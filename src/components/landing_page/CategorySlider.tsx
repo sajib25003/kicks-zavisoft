d0075875e1c03fc4583b5a206ff2a1e4fe7880b5
Md. Ashik Hassan Bhuiyan <sjb.25003@gmail.com>
2026-02-21 23:38:46 +0600

    category section responsive

--- src/components/landing_page/CategorySlider.tsx
+++ src/components/landing_page/CategorySlider.tsx
@@ -23,24 +23,24 @@ export default function CategoriesSlider() {
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div className="bg-[#232321] pl-6 md:pl-15 pt-16">
+    <div className="bg-[#232321] p-6 md:pl-15 md:pt-16">
       {/* Header */}
-      <div className="flex justify-between items-center mb-10">
-        <h2 className="text-white text-4xl md:text-6xl font-bold tracking-wide">
+      <div className="flex justify-between items-center mb-6 md:mb-10">
+        <h2 className="text-white text-2xl md:text-[74px] font-semibold tracking-wide">
           CATEGORIES
         </h2>
 
         {/* Custom Arrows */}
-        <div className="flex gap-4 items-center relative mr-16">
+        <div className="flex gap-4 items-center relative mr-4 md:mr-16">
           <div
-            className={`px-3.75 py-2.5 rounded-lg swiper-button-prev text-[#232321] bg-[#E7E7E3] rotate-180 ${
+            className={`px-2.5 py-1.5 md:px-3.75 md:py-2.5 rounded-lg swiper-button-prev text-[#232321] bg-[#E7E7E3] rotate-180 ${
               isBeginning ? "opacity-50 cursor-not-allowed" : "cursor-pointer"
             }`}
             onClick={() => swiper?.slidePrev()}
           ></div>
 
           <div
-            className={`px-3.75 py-2.5 rounded-lg swiper-button-next text-[#232321] bg-[#E7E7E3] ${
+            className={`px-2.5 py-1.5 md:px-3.75 md:py-2.5 rounded-lg swiper-button-next text-[#232321] bg-[#E7E7E3] ${
               isEnd ? "opacity-50 cursor-not-allowed" : "cursor-pointer"
             }`}
             onClick={() => swiper?.slideNext()}
@@ -67,7 +67,7 @@ export default function CategoriesSlider() {
           }}
           spaceBetween={0}
           breakpoints={{
-            0: { slidesPerView: 1 },
+            0: { slidesPerView: 2 },
             768: { slidesPerView: 2 },
             1024: { slidesPerView: 2 },
           }}
@@ -79,12 +79,14 @@ export default function CategoriesSlider() {
                   <img
                     src={item.image}
                     alt={item.name}
-                    className="w-full object-cover bg-[#F6F6F6]"
+                    className="w-full object-cover bg-[#F6F6F6] "
                   />
 
-                  <div className="mt-6 px-16 mb-7.5 flex justify-between items-center text-black">
-                    <h3 className="text-2xl font-semibold">{item.name}</h3>
-                    <div className="bg-[#232321] text-white w-8 aspect-square text-center rounded-lg flex justify-center items-center">
+                  <div className="my-4 md:mt-6 px-4 md:px-16 md:mb-7.5 flex justify-between items-center text-black w-full gap-4">
+                    <h3 className="text-xl md:text-2xl font-semibold">
+                      {item.name}
+                    </h3>
+                    <div className="bg-[#232321] text-white px-2 w-6 md:w-8 aspect-square text-center rounded-lg flex justify-center items-center">
                       ↗
                     </div>
                   </div>
